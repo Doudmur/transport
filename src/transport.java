e2a530cb61ab84a32c783b9de9f557301f2fc2e1
Doudmur <bbeka544@mail.ru>
2022-07-13 17:21:59 +0600

    first commit

---
+++ src/transport.java
@@ -0,0 +1,41 @@
+public abstract class transport {
+
+}
+
+class publicTransport extends transport{
+
+}
+
+interface ticketBuy{
+    void payTicket();
+}
+
+class bus extends publicTransport implements ticketBuy{
+    public void payTicket(){
+        System.out.println("Buy bus Ticket");
+    }
+}
+
+class trolleybus extends publicTransport implements ticketBuy{
+    public void payTicket() {
+        System.out.println("Buy trolley Ticket");
+    }
+}
+
+class minibus extends publicTransport implements ticketBuy{
+    public void payTicket() {
+        System.out.println("Buy minibus Ticket");
+    }
+}
+
+class armyTransport{
+
+}
+
+class tank extends armyTransport{
+    
+}
+
+
+
+
